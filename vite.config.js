3db6060de383bfde605bc149d32bf7051eb2c250
Semara-26 <148828331+Semara-26@users.noreply.github.com>
2025-04-16 19:07:19 +0800

    update vite.cofig.js

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/', // Ubah menjadi '/' karena ini adalah User/Organization Page
 })
